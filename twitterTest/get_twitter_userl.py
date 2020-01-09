c42384a122c1593805039cd4fc1d01f8d69e4d7c
westshellX_s30 <jia.dong.xing@163.com>
2020-01-09 20:30:55 +0800

    still can't download video

--- twitterTest/get_twitter_userl.py
+++ twitterTest/get_twitter_userl.py
@@ -1,71 +1,109 @@
-from twitterscraper.query import query_user_info
-import pandas as pd
-from multiprocessing import Pool
-from IPython.display import display
 import sys
+import os
+import traceback
+import json
+import time
+import csv
+import requests
+from requests.adapters import HTTPAdapter
+from twitterscraper import query_tweets_from_user
 
-global twitter_user_info
-twitter_user_info = []
+def validate_config(config):
+    # 验证user_id_list
+    user_id_list = config['user_id_list']
+    if (not isinstance(user_id_list,
+               list)) and (not user_id_list.endswith('.txt')):
+        sys.exit(u'user_id_list值应为list类型或txt文件路径')
+    if not isinstance(user_id_list, list):
+        if not os.path.isabs(user_id_list):
+            user_id_list = os.path.split(os.path.realpath(__file__))[0] + os.sep + user_id_list
+        if not os.path.isfile(user_id_list):
+            sys.exit(u'不存在%s文件' % user_id_list)
+def get_user_list(file_name):
+    """获取文件中的微博id信息"""
+    with open(file_name, 'rb') as f:
+        lines = f.read().splitlines()
+        lines = [line.decode('utf-8') for line in lines]
+        user_id_list = [
+            line.split(' ')[0] for line in lines
+            if len(line.split(' ')) > 0 
+        ]
+    return user_id_list
+def get_filepath(userName, type):
+    """获取结果文件路径"""
+    try:
+        file_dir = os.path.split(
+            os.path.realpath(__file__)
+        )[0] + os.sep + 'twitter_users_data' + os.sep + userName
+        if type == 'img' or type == 'video':
+            file_dir = file_dir + os.sep + type
+        if not os.path.isdir(file_dir):
+            os.makedirs(file_dir)
+        if type == 'img' or type == 'video':
+            return file_dir
+        file_path = file_dir + os.sep + userName + '_' + type
+        return file_path
+    except Exception as e:
+        print('Error: ', e)
+        traceback.print_exc()
+def download_one_file(userName,fileType,url):
+    """下载单个文件(图片/视频)"""
+    file_path=get_filepath(userName,fileType)
+    try:
+        if not os.path.isfile(file_path):
+            s = requests.Session()
+            s.mount(url, HTTPAdapter(max_retries=5))
+            downloaded = s.get(url, cookies='', timeout=(5, 10))
+            fileNameStart=url.rfind('/')
+            fileName=file_path+os.sep+url[fileNameStart+1:len(url)]
+            with open(fileName, 'wb') as f:
+                f.write(downloaded.content)
+    except Exception as e:
+        error_file = file_path + os.sep + 'not_downloaded.txt'
+        with open(error_file, 'ab') as f:
+            url = userName + ':' + url + '\n'
+            f.write(url.encode(sys.stdout.encoding))
+        print('Error: ', e)
+        traceback.print_exc() 
 
-def config_user():
-
-def get_user_info(twitter_user):
-    """
-    An example of using the query_user_info method
-    :param twitter_user: the twitter user to capture user data
-    :return: twitter_user_data: returns a dictionary of twitter user data
-    """
-    user_info = query_user_info(user=twitter_user)
-    twitter_user_data = {}
-    twitter_user_data["user"] = user_info.user
-    twitter_user_data["fullname"] = user_info.full_name
-    twitter_user_data["location"] = user_info.location
-    twitter_user_data["blog"] = user_info.blog
-    twitter_user_data["date_joined"] = user_info.date_joined
-    twitter_user_data["id"] = user_info.id
-    twitter_user_data["num_tweets"] = user_info.tweets
-    twitter_user_data["following"] = user_info.following
-    twitter_user_data["followers"] = user_info.followers
-    twitter_user_data["likes"] = user_info.likes
-    twitter_user_data["lists"] = user_info.lists
-
-    return twitter_user_data
-
-
-def main(args):
-  try:
-    config_path=os.path.split(os.path.realpath(__file__))[0]+os.sep+'config.json'
-	if not os.path.isfile(config_path):
-	    sys.exit(u'当前路径：%s 不存在配置文件config.json' %
-                     (os.path.split(os.path.realpath(__file__))[0] + os.sep))
+        
+def main():
+    try:
+        config_path=os.path.split(os.path.realpath(__file__))[0]+os.sep+ 'config.json'
+        if not os.path.isfile(config_path) :
+            sys.exit(u'当前路径：%s 不存在配置文件config.json' %(os.path.split(os.path.realpath(__file__))[0] + os.sep))
         with open(config_path) as f:
             config = json.loads(f.read())
-	users = []
+        validate_config(config)
+        
+        user_id_list = config['user_id_list']
+        if not isinstance(user_id_list, list):
+            if not os.path.isabs(user_id_list):
+                    user_id_list = os.path.split(os.path.realpath(__file__))[0] + os.sep + user_id_list
+            user_id_list =get_user_list(user_id_list)
+        for user in user_id_list:
+            print(user)
+            list_of_tweets=query_tweets_from_user(user,10)
+            outPutFileName=get_filepath(user,'data')+'.csv'
+            with open(outPutFileName,"w",encoding="utf-8") as output:
+                writer = csv.writer(output)
+                writer.writerow([
+                            "video_url", "reply_to_users"
+                        ])
+                for t in list_of_tweets:
+                    writer.writerow([
+                        t.video_url,t.reply_to_users
+                    ])
+                    for imgUrl in t.img_urls:
+                        download_one_file(user,'img',imgUrl)
+                    for videoUrl in t.video_url:
+                        download_one_file(user,'video',videoUrl)
+    except ValueError:
+        print('config.json格式不正确')
+    except Exception as e:
+        print('Error: ',e)
+        traceback.print_exe()
 
-    for arg in args:
-        users.append(arg)
-
-    pool_size = len(users)
-    if pool_size < 8:
-        pool = Pool(pool_size)
-    else:
-        pool = Pool(8)
-
-    for user in pool.map(get_user_info, users):
-        twitter_user_info.append(user)
-
-    cols = ['id', 'fullname', 'date_joined', 'location', 'blog', 'num_tweets', 'following', 'followers', 'likes',
-            'lists']
-    data_frame = pd.DataFrame(twitter_user_info, index=users, columns=cols)
-    data_frame.index.name = "Users"
-    data_frame.sort_values(by="followers", ascending=False, inplace=True, kind='quicksort', na_position='last')
-    display(data_frame)
-  except ValueError:
-    print('config.json格式不正确')
-  except Exception as e:
-    print('Error: ',e)
-	traceback.print_exe()
-	
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
 
