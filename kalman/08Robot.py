2918f12e7bb0bc05b5c7c59a23bc04d3c1df64dd
501homePC <jia.dong.xing@163.com>
2023-02-12 17:09:20 +0800

    NEES

--- kalman/08Robot.py
+++ kalman/08Robot.py
@@ -356,3 +356,104 @@ plot_residuals(xs[:, 0], data4, 0,
 plt.show();
 
 #Exercise: State Variable Design
+N = 30 # number of iterations
+dt = 1.0 # time step
+R_std = 0.35
+Q_std = 0.04
+
+M_TO_FT = 1 / 0.3048
+
+sensor = PosSensor((0, 0), (2, .5), noise_std=R_std)
+zs = np.array([sensor.read() for _ in range(N)])
+
+tracker = KalmanFilter(dim_x=4, dim_z=2)
+
+tracker.F = np.array([[1, 0, dt,  0],
+                      [0, 1,  0, dt],
+                      [0, 0,  1,  0],
+                      [0, 0,  0,  1]])
+
+tracker.H = np.array([[M_TO_FT, 0, 0, 0],
+                      [0, M_TO_FT, 0, 0]])
+
+tracker.R = np.eye(2) * R_std**2
+q = Q_discrete_white_noise(dim=2, dt=dt, var=Q_std**2)
+tracker.Q[0,0] = q[0,0]
+tracker.Q[1,1] = q[0,0]
+tracker.Q[2,2] = q[1,1]
+tracker.Q[3,3] = q[1,1]
+tracker.Q[0,2] = q[0,1]
+tracker.Q[2,0] = q[0,1]
+tracker.Q[1,3] = q[0,1]
+tracker.Q[3,1] = q[0,1]
+
+tracker.x = np.array([[0, 0, 0, 0]]).T
+tracker.P = np.eye(4) * 500.
+
+xs, ys = [], []
+for z in zs:
+    tracker.predict()
+    tracker.update(z)
+    xs.append(tracker.x[0])
+    ys.append(tracker.x[1])
+plt.plot(xs, ys);
+plt.show();
+
+from filterpy.common import kinematic_kf
+
+kf = kinematic_kf(dim=2, order=1, dt=1.0,dim_z=2, order_by_dim=False)
+kf.Q = np.diag([0, 0, .003, .003])
+kf.x = np.array([[1., 1., 0., 0.]]).T
+kf.R = np.diag([0.03, 0.21]) # use different errors
+
+for i in range(101):
+    kf.predict()
+    kf.update(np.array([[i*.05,i*.05]])) # around 200 kph
+
+p0 = kf.x[0:2]
+
+kf.predict()
+prior = kf.x
+z = kf.x[0:2]*2
+kf.update(z)
+p1 = kf.x[0:2]
+
+# compute error of measurement from prior
+y = np.abs(z - kf.H @ prior)
+dist = np.linalg.norm(y)
+
+np.set_printoptions(precision=2, suppress=True)
+
+print(f'bad measurement       : {z.T} km')
+print(f'before bad measurement: {p0.T} km')
+print(f'after bad measurement : {p1.T} km')
+print(f'estimate shift        : {np.linalg.norm(p1 - prior[:2]):.1f} km')
+print(f'distance from prior   : {dist:.1f} km')
+
+x, P = kf.x[0:2], kf.P[0:2, 0:2]
+plot_covariance_ellipse(x, P, std=[1,2,3]);
+plt.scatter(z[0], z[1], marker='x');
+plt.show();
+
+GATE_LIMIT = 4.
+std_x = np.sqrt(P[0,0])
+std_y = np.sqrt(P[1,1])
+y = kf.residual_of(z)[:,0]
+
+if y[0] > GATE_LIMIT * std_x or y[1] > GATE_LIMIT * std_y:
+    print(f'discarding measurement, error is {y[0]/std_x:.0f} std, {y[1]/std_y:.0f} std')
+    
+print('y   is', y)
+print(f'std is {std_x:.2f} {std_y:.2f}')
+
+plot_covariance_ellipse(x, P, std=[1,2,3])
+plt.scatter(8.08, 7.7, marker='x')
+plt.scatter(8.2, 7.65, marker='x');
+plt.show();
+
+from filterpy.stats import mahalanobis
+m = mahalanobis(x=z, mean=x, cov=P)
+print(f'mahalanobis distance = {m:.1f}');
+
+print(f'mahalanobis distance = {mahalanobis(x=[8.08, 7.7], mean=x, cov=P):.1f}')
+print(f'mahalanobis distance = {mahalanobis(x=[8.2, 7.65], mean=x, cov=P):.1f}')
