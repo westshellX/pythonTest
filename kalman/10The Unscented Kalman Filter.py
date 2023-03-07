1e415f95c3ce3b02d4ac3a78599d3ea61253f977
501homePC <jia.dong.xing@163.com>
2023-03-07 21:30:35 +0800

    The Unscented Transform

--- kalman/10The Unscented Kalman Filter.py
+++ kalman/10The Unscented Kalman Filter.py
@@ -25,3 +25,34 @@ def f(x):
     return (np.cos(4*(x/2 + 0.7))) - 1.3*x
 
 plot_nonlinear_func(data, f)
+
+N = 30000
+plt.subplot(121)
+plt.scatter(data[:N], range(N), alpha=.2, s=1)
+plt.title('Input')
+plt.subplot(122)
+plt.title('Output')
+plt.scatter(f(data[:N]), range(N), alpha=.2, s=1);
+plt.show();
+
+import numpy as np
+from numpy.random import multivariate_normal
+from kf_book.nonlinear_plots import plot_monte_carlo_mean
+
+def f_nonlinear_xy(x, y):
+    return np.array([x + y, .1*x**2 + y*y])
+
+mean = (0., 0.)
+p = np.array([[32., 15.], [15., 40.]])
+# Compute linearized mean
+mean_fx = f_nonlinear_xy(*mean)
+
+#generate random points
+xs, ys = multivariate_normal(mean=mean, cov=p, size=10000).T
+plot_monte_carlo_mean(xs, ys, f_nonlinear_xy, mean_fx, 'Linearized Mean');
+plt.show();
+
+from kf_book.ukf_internal import show_sigma_selections
+show_sigma_selections()
+
+#The Unscented Transform
