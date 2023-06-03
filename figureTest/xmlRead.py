13f9d820b12750523cc35935625dd31aa6df7c16
westshellX <jia.dong.xing@163.com>
2023-06-03 22:03:57 +0800

    尝试解析非标准xml文档

--- figureTest/xmlRead.py
+++ figureTest/xmlRead.py
@@ -1,1 +1,11 @@
 #打算读取shipData.xml的数据，然后进行分析比较
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+DOMTree=xml.dom.minidom.parse("C:\SMU\shipDynamicInfo.xml")
+
+collection=DOMTree.documentElement
+shipDatas=collection.getElementsByTagName("DynamicShipBase")
+for shipData in shipDatas:
+    #print(" %s" %shipData.getAttribute("x"))
+    print("hello")
